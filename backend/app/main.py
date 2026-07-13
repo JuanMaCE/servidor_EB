14ba2df6c6e23f85b1738fe547a10752a943df74
JuanMaCE <jk120365@gmail.com>
2026-07-13 12:31:45 -0600

    fix-- files fixed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,7 @@
+from fastapi import FastAPI
+from app import models
 from app.routers import songs
+
+app = FastAPI()
 
 app.include_router(songs.router)
---
+++ backend/app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.song import Song
+from app.models.artist import Artist
+from app.models.songArtist import SongArtist
