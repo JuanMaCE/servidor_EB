df32e5a1bbb94ed58d907db6d8245df7c28d6542
JuanMaCE <jk120365@gmail.com>
2026-07-15 11:36:19 -0600

    update --song endpoints are completed

--- backend/app/routers/songs.py
+++ backend/app/routers/songs.py
@@ -19,4 +19,40 @@ def create_song(song: SongCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_song)
     return new_song
+
+@router.get("/", response_model=List[SongResponse], status_code=status.HTTP_200_OK)
+def get_songs(skip: int = 0, limit: int = 10, db):
+    songs = db.query(Song).offset(skip).limit(limit).all()
+    return songs
+
+@router.get("/{song_id}", response_model=SongResponse, status_code=status.HTTP_200_OK)
+def get_song(song_id: int, db: Session = Depends(get_db)):
+    song = db.query(Song).filter(Song.id == song_id).first()
+    if not song:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Song not found")
+    return song
+
+
+@router.put("/{song_id}", response_model=SongResponse, status_code=status.HTTP_200_OK)
+def update_song(song_id: int, song_update: SongCreate, db: Session = Depends(get_db)):
+    song = db.query(Song).filter(Song-id == song_id).first()
+    if not song:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Song not found")
+    for key, value in song_update.model_dump().items():
+        setattr(song, key, value)
+    
+    db.commit()
+    db.refresh(song)
+    return song
+
+
+@router.delete("/{song_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_song(song_id: int, db: Session = Depends(get_db)):
+    song = db.query(Song).filter(Song.id == song_id).first()
+    if not song:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Song not found")
+    db.delete(song)
+    db.commit()
+    return None 
+
     
